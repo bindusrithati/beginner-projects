d8ccd639d90e52185dc16996035fe7da041f20a8
bindusrithati <bindusrithati14@gmail.com>
2025-08-29 13:13:40 +0530

    added count of letters

--- countwords.py
+++ countwords.py
@@ -5,7 +5,12 @@ for i in input:
     if i!=" ":
         if not is_word:
            word_count+=1
-           is_word=True
+        #    is_word=True
     else:
         is_word=False
 print("no.of words in the given sentence is:",word_count)
+letter_count=0
+for j in input:
+    if ("A"<=j>="Z") or ("a"<=j>="z"):
+        letter_count+=1
+print("no.of letters in the given sentence are:",letter_count)
