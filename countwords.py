e483b78f88b7d1297a61d28517078c8ab51883a3
bindusrithati <bindusrithati14@gmail.com>
2025-08-30 11:09:26 +0530

    first word non repeating

--- countwords.py
+++ countwords.py
@@ -5,7 +5,7 @@ for i in input:
     if i!=" ":
         if not is_word:
            word_count+=1
-        #    is_word=True
+           is_word=True
     else:
         is_word=False
 print("no.of words in the given sentence is:",word_count)
